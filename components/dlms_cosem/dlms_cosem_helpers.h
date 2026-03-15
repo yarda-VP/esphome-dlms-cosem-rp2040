60a3ad54c37d7ced4fcc09aec39e3a8bfbfe868f
yarda-VP <yarda@post.cz>
2026-03-15 17:19:48 +0100

    Update dlms_cosem_helpers.h

--- components/dlms_cosem/dlms_cosem_helpers.h
+++ components/dlms_cosem/dlms_cosem_helpers.h
@@ -4,14 +4,15 @@
 #include <dlmssettings.h>
 
 #include <vector>
+
+namespace esphome {
+namespace dlms_cosem {
+
 // Rozdělí "a; b ;c;;  d" → ["a","b","c","d"]
 // - oddělovač: ';'
 // - prázdné položky ignoruje
 // - ořezává bílé znaky kolem položek
 void split_semicolon_list(const std::string &src, std::vector<std::string> &out);
-
-namespace esphome {
-namespace dlms_cosem {
 
 float dlms_data_as_float(DLMS_DATA_TYPE value_type, const uint8_t *value_buffer_ptr, uint8_t value_length);
 std::string dlms_datetime_as_string(const uint8_t *value_buffer_ptr, uint8_t value_length);
